35b9935d28e9f10e60b6593c126729a4ce5073d2
muna <munadiab85@gmail.com>
2023-07-17 15:12:42 +0300

    c

--- calculator/Button.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-//import "./Button.css";
-
-function clearOp() {
-    document.getElementById("result").value = "";
-}
--- calculator/ButtonPanel.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-//import "./ButtonPanel.css";
-function butt(value) {
-    document.getElementById("result").value += value;
-}
